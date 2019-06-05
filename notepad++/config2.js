0ee22d8583705d6f5128178077036e789b9aaa8c
klopezpa <imaginesalescr@gmail.com>
2019-06-05 09:57:23 -0600

    Update and rename config.js to config2.js
    #### THIS FILE IS TO SOLVE THE PATH ISSUE.
    ## System path not found #5

--- notepad++/config2.js
+++ notepad++/config2.js
@@ -8,8 +8,12 @@ config.command = () => new Promise(resolve => {
   chrome.storage.local.get({
     Mac: 'open -a "Notepad++" %path;',
     Lin: '/opt/notepadpp/notepadpp %path;',
-    Win: '"%ProgramFiles(x86)%\\Notepad++\\notepad++.exe" %path;'
+    Win: '"C:\Program Files\Notepad++\notepad++.exe" %path;'
   }, prefs => {
     resolve(prefs[navigator.platform.substr(0, 3)]);
   });
 });
+
+
+#### THIS FILE IS TO SOLVE THE PATH ISSUE.
+## System path not found #5
